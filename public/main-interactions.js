b544b9b43dcfcd30946795cc85f0a5bde85acc42
SirCumsALot779I <aramis.seitz@gmail.com>
2025-08-05 23:43:21 +0200

    Update main-interactions.js

--- public/main-interactions.js
+++ public/main-interactions.js
@@ -1,25 +1,6 @@
 const menuButton = document.getElementById('menuButton');
 const dropdown = document.getElementById('dropdown');
 
-const messages = [
-  "Willkommen, fauler sack",
-  "solltest du nicht lernen?",
-  "ich werte das mal als ein 'ja'",
-  "mach nur so weiter dann siehst du bald so aus wie aramis",
-  "überarbeite dich nicht gleich",
-  "wenn ich so viel machen würde wie du wär ich auch am schwitzen ahh ne du kommst grad nur vom klo",
-  "dein dorf in Clash of Clans wird angegriffen",
-  "ja jz hab ich deine aufmerksamkeit, das war klar",
-  "du musst schon mit lernen Anfangen um hier irgendwas geschissen zu bekommen",
-  "ich seh schon das wird in ein Romeo speedrun (der text is obviously von laura)",
-  "uhh Michael hab ich heute schon erwäht wie gut sie aussehen, während sie diese Aufgabe korrigiern!",
-  "das ist Text. Ich hab doch auch kein plan was hier noch stehen kann, geh doch bitte einfach lernen du **** ***** ***** ******",
-  "Hier könnte ihre Werbung stehen!"
-];
-
-const menuButton = document.getElementById('menuButton');
-const dropdown = document.getElementById('dropdown');
-
 menuButton.addEventListener('click', () => {
   const isVisible = dropdown.classList.toggle('visible');
   menuButton.setAttribute('aria-expanded', isVisible);
@@ -52,6 +33,22 @@ document.addEventListener('keydown', (e) => {
     menuButton.focus();
   }
 });
+
+const messages = [
+  "Willkommen, fauler sack",
+  "solltest du nicht lernen?",
+  "ich werte das mal als ein 'ja'",
+  "mach nur so weiter dann siehst du bald so aus wie aramis",
+  "überarbeite dich nicht gleich",
+  "wenn ich so viel machen würde wie du wär ich auch am schwitzen ahh ne du kommst grad nur vom klo",
+  "dein dorf in Clash of Clans wird angegriffen",
+  "ja jz hab ich deine aufmerksamkeit, das war klar",
+  "du musst schon mit lernen Anfangen um hier irgendwas geschissen zu bekommen",
+  "ich seh schon das wird in ein Romeo speedrun (der text is obviously von laura)",
+  "uhh Michael hab ich heute schon erwäht wie gut sie aussehen, während sie diese Aufgabe korrigiern!",
+  "das ist Text. Ich hab doch auch kein plan was hier noch stehen kann, geh doch bitte einfach lernen du **** ***** ***** ******",
+  "Hier könnte ihre Werbung stehen!"
+];
 
 
 const changeMessageButton = document.getElementById('changeMessageButton');
